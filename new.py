90d70a9826cf2de9159e6ded7131d85fe98ae36d
SavinMadapatha <86455560+SavinMadapatha@users.noreply.github.com>
2021-07-01 13:04:59 +0530

    Update new.py

--- new.py
+++ new.py
@@ -1,14 +1,1 @@
-import time;
-print("Ranushi :",end="")
-time.sleep(0.5)
-string="mn oyt adrei savin baba"
-for i in string:
-    print(i,end="")
-    time.sleep(0.2)
-time.sleep(0.5)
-print("\nSavin : ",end="")
-string = "maath adrei ranushi manika"
-for i in string:
-    print(i,end="")
-    time.sleep(0.2)
-    
+print("Welcome to GITHUB")

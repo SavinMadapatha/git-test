00845a2c5adb6e49d40afe82c2603d1f4af02656
SavinMadapatha <86455560+SavinMadapatha@users.noreply.github.com>
2021-07-01 12:39:03 +0530

    I have declared 4 variables

--- Collabtest1.java
+++ Collabtest1.java
@@ -1,1 +1,8 @@
-Edit karamutheeee
+public class Collabtest {
+  public static void main(String [] args) {
+    String member1 = "Savin";
+    String member2 = "Uvindu";
+    String member3 = "Chamath";
+    String member4 = "Chathumi";
+  }
+}
